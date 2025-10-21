16406345e61e55020005fd27198fc94cf140300f
Kaburi <specialemail90@gmail.com>
2025-10-22 00:37:41 +0900

    Velopack 적용

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,8 @@ using SpeechAgent.Features.Settings;
 using SpeechAgent.Services;
 using SpeechAgent.Services.MedicSIO;
 using System.Windows;
+using Velopack;
+using Velopack.Sources;
 
 namespace SpeechAgent
 {
@@ -40,6 +42,45 @@ namespace SpeechAgent
     public App()
     {
       Services = ConfigureServices();
+    }
+
+    private static async Task UpdateMyApp()
+    {
+#if !DEBUG
+      var mgr = new UpdateManager("https://github.com/KaburiCoder/SpeechAgent/releases/latest/download");
+
+      // check for new version
+      try
+      {
+        var newVersion = await mgr.CheckForUpdatesAsync();        
+        if (newVersion == null)
+          return; // no update available
+                  // download new version
+        await mgr.DownloadUpdatesAsync(newVersion);
+
+        // install new version and restart app
+        mgr.ApplyUpdatesAndRestart(newVersion);
+      }
+      catch (Exception err)
+      {
+        System.Windows.MessageBox.Show(err.ToString());
+      }
+#else
+      await Task.CompletedTask;
+#endif
+    }
+
+    [STAThread]
+    public static void Main()
+    {
+      // Velopack을 진입점에서 가장 먼저 실행
+      VelopackApp.Build().Run();
+      _ = UpdateMyApp();
+
+      // WPF Application 실행
+      var app = new App();
+      app.InitializeComponent();
+      app.Run();
     }
 
     private void Application_Startup(object sender, StartupEventArgs e)
---
+++ README.md
@@ -0,0 +1,18 @@
+﻿# Velopack 배포
+
+https://docs.velopack.io/getting-started/csharp?platform=wpf
+
+**publish**
+```
+dotnet publish SpeechAgent.csproj --self-contained -r win-x64 -o .\publish
+```
+
+**pack**
+```
+vpk pack --packId SpeechAgent --packVersion 0.0.2 --packDir .\publish --mainExe SpeechAgent.exe
+```
+
+**publish github release**
+```
+vpk upload github --repoUrl https://github.com/KaburiCoder/SpeechAgent --publish --releaseName "SpeechAgent 0.0.2" --tag v0.0.2 --token ghp_
+```

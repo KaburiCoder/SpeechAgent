ad6307f3dbcbaf20b011e286522719718dd933f0
Kaburi <specialemail90@gmail.com>
2025-10-26 17:28:10 +0900

    컨트롤 조회 렌더링 속도 개선

--- Constants/UpdateConfig.cs
+++ Constants/UpdateConfig.cs
@@ -2,7 +2,7 @@
 {
   public class UpdateConfig
   {
-    public const string UpdateUrlX64 = "https://github.com/KaburiCoder/SpeechAgent_x64/releases/latest/download";
-    public const string UpdateUrlX86 = "https://github.com/KaburiCoder/SpeechAgent_x32/releases/latest/download";
+    public const string UpdateUrlX64 = "https://github.com/clickcns/SpeechAgent_x64/releases/latest/download";
+    public const string UpdateUrlX86 = "https://github.com/clickcns/SpeechAgent_x32/releases/latest/download";
   }
 }
